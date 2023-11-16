7b1318856cbbf97cba0ccabab1c59e7a578b5db8
jmr90 <jonathanmartinez@Jonathans-MacBook-Pro.local>
2023-11-16 16:32:54 -0500

    troubleshooting elements added

--- javascript/index.js
+++ javascript/index.js
@@ -8,18 +8,22 @@ function validateInputs() {
     var phoneFilter = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/
 
     if (fullname.length < 1 || email.length < 1 || phone.length < 1) {
+        console.log("inputs not filled in");
         formErrors = "All fields are required. Please complete the form.";
         submitForm = false;
     } else {
         if (!emailFilter.test(email) || !phoneFilter.test(phone)) {
+            console.log("input not formatted");
             formErrors = "Your form contains invalid field entries. Please correct your form before submitting";
             submitForm = false;
         }
     }
     if (submitForm == false) {
+        console.log("submitForm was false, submit prevented");
         alert(formErrors);
         return false;
     } else {
+        console.log("form submitted");
         fullname = fullname.replace(/[^a-z0-9\s\-]/gim,"");
         fullname = fullname.trim();
         email = email.replace(/[^a-z0-9_@.-]/gim,"");
